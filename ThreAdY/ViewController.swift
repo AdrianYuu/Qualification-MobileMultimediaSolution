6ed2c83517d00ca5c95c01585584dce718b7ad52
AdrianYuu <adrianyuu01@gmail.com>
2024-08-20 10:18:42 +0700

    Initial Commit

---
+++ ThreAdY/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreAdY
+//
+//  Created by Adrian Yu on 20/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
